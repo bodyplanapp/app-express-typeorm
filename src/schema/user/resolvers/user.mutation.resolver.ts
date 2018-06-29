a9b22c6a23e35ae9c60b53024f5585f6a3d97075
eretamero <eric2reta@gmail.com>
2018-06-29 13:15:54 +0200

    refactor

--- src/schema/user/resolvers/user.mutation.resolver.ts
+++ src/schema/user/resolvers/user.mutation.resolver.ts
@@ -3,10 +3,13 @@ import { User } from "../../../models/user.model";
 
 export const Mutation = {
   async createUser(_, { user: attrs }) {
-    console.log(attrs)
     const user = new User();
     user.firstName = attrs.firstName;
     user.lastName = attrs.lastName;
+    user.email = attrs.email;
+    user.password = attrs.password
+    user.username = attrs.username;
+    user.lang = attrs.lang;
     return await getRepository(User).save(user);
   }
 };
